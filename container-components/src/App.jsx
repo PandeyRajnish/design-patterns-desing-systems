f45e95a284c3b705ee6684229db53fb43bdd8e9a
Rajnish Kumar <rajnish.pandey@costrategix.com>
2026-08-25 14:57:11 +0530

    Add user IDs to users and books data structures, and update App component to include multiple UserLoader instances for displaying user information.

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,12 +1,27 @@
 import { CurrentUserLoader } from "./components/current-user-loader";
 import { UserInfo } from "./components/user-info";
+import { UserLoader } from "./components/user-loader";
 
 function App() {
   return (
     <>
+      {/*  current user */}
       <CurrentUserLoader>
         <UserInfo />
       </CurrentUserLoader>
+
+      {/* user loader */}
+      <UserLoader userId={"3"}>
+        <UserInfo />
+      </UserLoader>
+
+      <UserLoader userId={"2"}>
+        <UserInfo />
+      </UserLoader>
+
+      <UserLoader userId={"1"}>
+        <UserInfo />
+      </UserLoader>
     </>
   );
 }
